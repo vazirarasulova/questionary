2fd4612bb01eb5b29ed177f9b53ae9c443668128
vazirarasulova <rasulovavazira202gmail.com>
2022-04-28 15:45:59 +0500

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var firstName = prompt("Ismingizni kiriting")
+var firstQuestion = prompt("Bootstrapda nechi xil breakpoints turlari bor?"); // 6 xil
+var secondQuestion = prompt("Dropdown-menu classi htmldagi qaysi tagda yoziladi?") // ul
+var thirdQuestion = prompt("Alertning Warning holatini yaratish uchun classlar ketma ketligini yozing") // alert alert-warning
+var fourthQuestion = prompt(".container-lg nechidan-nechigacha bo'lgan withlarni o'z ichiga oladi") // 992-1200
+var fifthQuestion = prompt(".gx-* classining to'liq nomini kiriting")   // horizontal gutter widths.
+var sixthQuestion = prompt("Bootstrapda imagelarni qaysi class orqali responsive qilish mumkin?")   //.img-fluid. 
+var seventhQuestion = prompt("Imagega 1px border radius berish uchun qaysi class ishlatiladi") //.img-thumbnai
+var eighthQuestion = prompt("Tablening borderini olib tashlash uchun bootstrap classini yozing") //.table-borderless
+var ninethQuestion = prompt("Alertning barcha classlarini yozing") // class="alert alert-primary" role="alert"
+var tenthQuestion = prompt("Badge classi htmlning qaysi (tag)-ida yoziladi") // span
+
+console.log(firstName + "\n" + firstQuestion + "\n" + secondQuestion + "\n" + thirdQuestion + "\n" + fourthQuestion + "\n" + fifthQuestion + "\n" + sixthQuestion + "\n" + seventhQuestion + "\n" + eighthQuestion + "\n" + ninethQuestion + "\n" + tenthQuestion);
